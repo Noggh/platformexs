06dd755a15339676d5cbd597c1f1b9fa554d8f4d
Guilherme Nogueira <nogr.gh@gmail.com>
2023-12-24 03:18:38 -0300

    Adding specs to tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,6 +1,5 @@
-{-# LANGUAGE OverloadedStrings #-}
-
 import           BobSpec             (bobSpecs)
+import           LuhnSpec            (luhnSpecs)
 import           Exercism.HelloWorld (hello)
 import           Test.Hspec          (Spec, describe, it, shouldBe)
 import           Test.Hspec.Runner   (configFailFast, defaultConfig, hspecWith)
@@ -10,11 +9,5 @@ main = hspecWith defaultConfig {configFailFast = True} mainSpecs
 
 mainSpecs :: Spec
 mainSpecs = do
-    exercismSpecs
     bobSpecs
-
-exercismSpecs :: Spec
-exercismSpecs = do
-    describe "hello" $ do
-        it "is \"Hello, World!\"" $ do
-            hello `shouldBe` "Hello, World!"
+    luhnSpecs
