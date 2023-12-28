5b811b0340ab548bccec1f2481e9fc1f0a65d5c6
Guilherme Nogueira <nogr.gh@gmail.com>
2023-12-28 01:56:57 -0300

    Fixing errors

--- src/Exercism/Wordy.hs
+++ src/Exercism/Wordy.hs
@@ -9,23 +9,24 @@
 module Exercism.Wordy (answer) where
 
 import           Control.Monad              (void)
-import           Data.Text                  (Text)
+import           Data.Text                  (Text, pack)
 import           Data.Void                  (Void)
-import           Text.Megaparsec            (MonadParsec (try), Parsec, choice, parseMaybe)
+import           Text.Megaparsec            (MonadParsec (try), Parsec, choice,
+                                             parseMaybe, parseTest)
 import           Text.Megaparsec.Char       (space1, string')
 import qualified Text.Megaparsec.Char.Lexer as L
 
 type Parser = Parsec Void Text
 
-answer t = parseMaybe parseInput t >>= \case Return s -> Just s
+answer t = parseMaybe parseInput (pack t) >>= \case Return s -> Just $ toInteger s
 
 lexeme  = L.lexeme parseSpace
-integer = lexeme L.decimal
+integer = lexeme $ L.signed parseSpace L.decimal
 
 parseInput :: Parser (Comp Int)
 parseInput = try $ do
     void $ lexeme $ string' "what is"
-    foldr1 (<**>) <$> parseOpsUntilEnd
+    foldl1 (<**>) <$> parseOpsUntilEnd
 
 parseOpsUntilEnd = go []
     where go xs = do
@@ -34,16 +35,16 @@ parseOpsUntilEnd = go []
                          _        -> go $ comp : xs
 
 parsePattern = do
-    s <- integer
+    s <- lexeme integer
     parseOneOp >>= \case Nothing -> pure . Return $ s
                          Just o  -> pure . Perf s $ o
 
 parseOneOp = choice
-    [ Just Plus     <$ string' "plus"
-    , Just Subtract <$ string' "subtract"
-    , Just Multiply <$ string' "multiply"
-    , Just Divide   <$ string' "divide"
-    , Nothing       <$ string' "?"
+    [ Just Plus         <$ string' "plus"
+    , Just Minus        <$ string' "minus"
+    , Just MultipliedBy <$ string' "multiplied by"
+    , Just DividedBy    <$ string' "divided by"
+    , Nothing           <$ string' "?"
     ]
 
 parseSpace = L.space space1 (L.skipLineComment "//") (L.skipBlockComment "/*" "*/")
@@ -58,11 +59,11 @@ data Comp a where
 (Perf s op) <**> (Perf s' op') = flip Perf op' $ run op s s'
 
 run :: (Integral a) => Operation -> (a -> a -> a)
-run Plus     = (+)
-run Subtract = (-)
-run Multiply = (*)
-run Divide   = div
+run Plus         = (+)
+run Minus        = (-)
+run MultipliedBy = (*)
+run DividedBy    = div
 
-data Operation = Plus | Multiply | Subtract | Divide | End
+data Operation = Plus | MultipliedBy | Minus | DividedBy | End
     deriving (Show)
 
